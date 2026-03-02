86c8edcd9aa7b1cd6a40985cbae6e641f5f4f928
dhruv kumar <dhruvsingh200420@gmail.com>
2026-03-02 12:42:23 +0530

    final ai task manager

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
 import { TaskProvider } from "./context/TaskContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <TaskProvider>
-    <App />
-  </TaskProvider>
+  <React.StrictMode>
+    <TaskProvider>
+      <App />
+    </TaskProvider>
+  </React.StrictMode>
 );
